07c2d1b7d51af00d8b4afcdfb6bb1cc297a0a52f
pitnaree <pitnaree_@outlook.com>
2026-08-13 20:23:43 +0700

    feat:create panel for profile update page

--- src/app/owner/profile/page.js
+++ src/app/owner/profile/page.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function OwnerProfilePage() {
   return (
-    <div>
-      <h1>Owner Profile</h1>
+    <div className="flex flex-row bg-gray-100 h-screen">
+      <div className="flex flex-col mt-20 m-4 gap-2 bg-white rounded-lg p-4 shadow-md w-1/3">
+        <h4>Account</h4>
+      </div>
+      <div className="flex flex-col mt-20 m-4 p-4 bg-white rounded-lg shadow-md w-2/3">
+        <div className="flex flex-col gap-2">
+          <h4 className="text-2xl font-bold">Profile</h4>
+        </div>
+      </div>
+
     </div>
   );
 }
