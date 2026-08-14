cc8d236f4df70dfad2f6dc8ca4abfe5379f73876
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-14 13:17:24 +0700

    feat: implement global footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata = {
   title: "Pet Sitter App",
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col font-sans bg-gray-50">
         <Navbar />
         <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
+
