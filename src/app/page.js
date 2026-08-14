b748a6475a5d8e6b4abcb2a4e56a94bc28816715
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-14 16:51:56 +0700

    feat: remove Navbar and add ContentSection and CTASection to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import ContentSection from "../components/ContentSection";
+import CTASection from "../components/CTASection";
 
 export default function Home() {
     return (
         <main className="min-h-screen bg-white">
-            <Navbar />
             <HeroSection />
+            <ContentSection />
+            <CTASection />
         </main>
     );
 }
