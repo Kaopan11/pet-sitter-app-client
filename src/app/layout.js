6f80e26f39b6df4a05782f4df7279147c3e42a6d
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-14 14:50:11 +0700

    refactor(layout): remove Navbar and simplify RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col font-sans bg-gray-50">
-        <Navbar />
-        <main className="flex-1">{children}</main>
-      </body>
-    </html>
+   <></>
   );
 }
