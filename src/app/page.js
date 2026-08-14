5b19ee0568765c2bf075cdde29c374d84147eb0b
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-14 10:05:57 +0700

    feat: add ContentSection component to homepage with service descriptions and feature cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import HeroSection from "./components/HeroSection";
+import ContentSection from "./components/ContentSection";
 
 export default function Home() {
     return (
         <div>
             <HeroSection />
+            <ContentSection />
         </div>
     );
 }
+
